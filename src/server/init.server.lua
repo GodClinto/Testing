ec9245540240edfd69949c5d02b1a01ba984a5ea
GodClinto <clint@yongoueth.com>
2021-05-31 17:47:55 -0500

    Testing

---
+++ src/server/init.server.lua
@@ -0,0 +1,8 @@
+while true do
+    game.Workspace.Baseplate.BrickColor = BrickColor.Black()
+    wait(5)
+    game.Workspace.Baseplate.BrickColor = BrickColor.Blue()
+    wait(5)
+    game.Workspace.Baseplate.BrickColor = BrickColor.White()
+    wait(5)
+end
