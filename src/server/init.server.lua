22ba785b971399f836c62eda12dec8f2d24782af
GodClinto <clint@yongoueth.com>
2021-05-31 19:35:31 -0500

    Deletion

--- src/server/init.server.lua
+++
@@ -1,8 +0,0 @@
-while true do
-    game.Workspace.Baseplate.BrickColor = BrickColor.Black()
-    wait(5)
-    game.Workspace.Baseplate.BrickColor = BrickColor.Blue()
-    wait(5)
-    game.Workspace.Baseplate.BrickColor = BrickColor.White()
-    wait(5)
-end
